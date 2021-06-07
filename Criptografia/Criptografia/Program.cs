a045680a386dcd1e17c11bac2525ff16533028c5
ferreiraspe <ferreiraspe@gmail.com>
2021-06-07 09:45:54 -0300

    Implementing cripto

---
+++ Criptografia/Criptografia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criptografia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var teste1 = CriptografiaNovo.Seguranca.Criptografar("Teste Criptografia", "@@teste44");
+
+            var valor = "UUVCMFpYTjBaVFEwS2lvcUtpcFdSMVo2WkVkVloxRXpTbkJqU0ZKMldqTkthRnB0YkdnPQ==";
+
+            var respostaTeste = 
+                CriptografiaNovo.Seguranca.Descriptografar("UUVCMFpYTjBaVFEwS2lvcUtpcFdSMVo2WkVkVloxRXpTbkJqU0ZKMldqTkthRnB0YkdnPQ==", "@@teste44");
+        }
+    }
+}
